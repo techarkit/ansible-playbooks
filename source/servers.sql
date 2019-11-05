7d1581735057593f7e6ec035fce543b748574f6e
Ankam Ravi Kumar <techtutorials@users.noreply.github.com>
2019-11-05 17:55:21 +0530

    Update servers.sql

--- source/servers.sql
+++ source/servers.sql
@@ -10,3 +10,13 @@ CREATE TABLE IF NOT EXISTS `servers` (
   `remarks` varchar(255) NOT NULL,
   PRIMARY KEY (`sn`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+
+--
+-- Dumping data for table `servers`
+--
+
+INSERT INTO `servers` (`sn`, `servname`, `ip`, `location`, `env`, `remarks`) VALUES
+(1, 'ansible-master.techarkit.local', '192.168.2.200', 'India', 'Development', 'Ansible Development Master'),
+(2, 'node2.techarkit.local', '192.168.2.201', 'India', 'Development', 'Ansible Node'),
+(3, 'node3.techarkit.local', '192.168.2.202', 'India', 'Development', 'Ansible Node');
